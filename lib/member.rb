7bbe89eca394a46823ac8255978baff07380bd15
Victor Allen <vamuigua@gmail.com>
2017-07-07 12:21:09 +0300

    Added method to return the correct id of a member

--- lib/member.rb
+++ lib/member.rb
@@ -10,5 +10,11 @@ class Member
       @name = attributes.fetch(:name)
       @id = @@members.length().+(1)
     end
+    
+    #method id to return the id of a member
+    define_method(:id) do
+      @id
+    end
+    
 end
   
