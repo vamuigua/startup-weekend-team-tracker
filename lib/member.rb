22095cee461a70ff23403f3ea02ae994187b2535
Victor Allen <vamuigua@gmail.com>
2017-07-07 12:20:10 +0300

    Added initialize variables to be used when creating a new member of a group

--- lib/member.rb
+++ lib/member.rb
@@ -4,5 +4,11 @@ class Member
     
     #array to store members
     @@members = []
+    
+    #initialize variables
+    define_method(:initialize) do |attributes|
+      @name = attributes.fetch(:name)
+      @id = @@members.length().+(1)
+    end
 end
   
