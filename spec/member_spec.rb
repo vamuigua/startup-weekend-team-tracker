92d2cb79fc931524049ebd0e85f6ec5e22a1d801
Victor Allen <vamuigua@gmail.com>
2017-07-07 12:28:47 +0300

    Added spec to check the name of the member

--- spec/member_spec.rb
+++ spec/member_spec.rb
@@ -1,3 +1,11 @@
 require('rspec')
 require('team')
 require('member')
+
+#spec to return the name of a member of a team
+describe("#name") do
+    it("returns the name of the member") do
+        test_member = Member.new({:name=>"Joneh"})
+        expect(test_member.name()).to(eq("Joneh"))
+    end
+end
