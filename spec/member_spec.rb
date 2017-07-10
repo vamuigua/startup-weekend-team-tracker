3a86b46ab3442b2070a94e9174f5cfe019937d23
Victor Allen <vamuigua@gmail.com>
2017-07-10 14:56:10 +0300

    Updated test specs

--- spec/member_spec.rb
+++ spec/member_spec.rb
@@ -2,11 +2,11 @@ require('rspec')
 require('team')
 require('member')
 
-#spec to return the name of a member of a team
-describe("#name") do
-    it("returns the name of the member") do
-        test_member = Member.new({:name=>"Joneh"})
-        expect(test_member.name()).to(eq("Joneh"))
+#spec to return the member_name of a member of a team
+describe("#member_name") do
+    it("returns the member_name of the member") do
+        test_member = Member.new({:member_name=>"June"})
+        expect(test_member.member_name()).to(eq("June"))
     end
 end
 
@@ -20,7 +20,7 @@ end
 #spec to save new members
 describe("#save") do
     it("adds a member to the team of saved members") do
-        test_member = Member.new({:name=>"Joneh"})
+        test_member = Member.new({:member_name=>"June"})
         test_member.save()
         expect(Member.all()).to(eq([test_member]))
     end
@@ -29,7 +29,7 @@ end
 #spec to clear the members in a team
 describe(".clear") do
     it("empties out all of the saved members from the team") do
-        Member.new({:name=>"Joneh"}).save()
+        Member.new({:member_name=>"June"}).save()
         Member.clear()
         expect(Member.all()).to(eq([]))
     end
@@ -38,7 +38,7 @@ end
 #spec to give the id of the member in a team
 describe("#id") do
     it("returns the id of the mamber") do
-        test_member = Member.new({:name=>"Joneh"})
+        test_member = Member.new({:member_name=>"June"})
         test_member.save()
         expect(test_member.id()).to(eq(1))
     end
@@ -47,9 +47,9 @@ end
 #spec to find each member of a team by the id
 describe(".find") do
     it("returns a member of a team by its id number") do
-        test_member = Member.new({:name=>"Joneh"})
+        test_member = Member.new({:member_name=>"June"})
         test_member.save()
-        test_member2 = Member.new({:name=>"June"})
+        test_member2 = Member.new({:member_name=>"June"})
         test_member2.save() 
         expect(Member.find(test_member.id())).to(eq(test_member))
     end
