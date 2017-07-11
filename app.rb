d87c07eed167e1724dc5a4f3c30a558552d58acf
Victor Allen <vamuigua@gmail.com>
2017-07-11 14:34:37 +0300

    Added routes to members and teams both post and get requests

--- app.rb
+++ app.rb
@@ -9,3 +9,41 @@ require('pry')
 get('/') do
     erb(:index)
   end
+
+#route to new team form
+get("/teams/new") do
+	erb(:team_form)
+end
+
+#post request to post the teams
+post("/teams") do
+	team_name = params.fetch("team_name")
+	new_team = Team.new({:team_name=>team_name})
+	new_team.save()
+	erb(:success)
+end
+
+#get request to display teams added
+get("/teams") do
+	@teams = Team.all()
+	erb(:teams)
+end
+
+#route to new member form
+get("/members/new") do
+	erb(:member_form)
+end
+
+#post request to post members
+post("/members") do
+	member_name = params.fetch("member_name")
+	new_member = Member.new({:member_name=>member_name})
+	new_member.save()
+	erb(:success)
+end
+
+#get request to display members added
+get("/members") do
+	@members = Members.all()
+	erb(:members)
+end
